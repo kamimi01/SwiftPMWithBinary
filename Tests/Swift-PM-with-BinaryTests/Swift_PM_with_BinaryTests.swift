cb7b240988b60b7894fcdff26804a1d9d198a24d
mika.urakawa <awakaru0122@gmail.com>
2023-06-15 09:50:36 +0900

    feat: create new SwiftPM with a binary

---
+++ Tests/Swift-PM-with-BinaryTests/Swift_PM_with_BinaryTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Swift_PM_with_Binary
+
+final class Swift_PM_with_BinaryTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Swift_PM_with_Binary().text, "Hello, World!")
+    }
+}
